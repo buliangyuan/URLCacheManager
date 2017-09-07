0edfaf538110cf5557f2dea5a8f793ab6714685c
huangzugang <huangzugang@51yjtc.com>
2017-09-07 14:26:10 +0800

    1. URLCacheManager.framework 0.0.1

---
+++ URLCacheManager.framework/Headers/URLCacheManager.h
@@ -0,0 +1,20 @@
+//
+//  URLCacheManager.h
+//  URLCacheManager
+//
+//  Created by hzg on 2017/9/7.
+//  Copyright © 2017年 yjtc. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for URLCacheManager.
+FOUNDATION_EXPORT double URLCacheManagerVersionNumber;
+
+//! Project version string for URLCacheManager.
+FOUNDATION_EXPORT const unsigned char URLCacheManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLCacheManager/PublicHeader.h>
+#import "sqlite3.h"
+
+
